4ff71f23f0646da8c90da14cfe1fd1ff5844f84e
Andre <andre.d.oliveira@tecnico.ulisboa.pt>
2023-11-28 15:06:29 +0000

    novas vibes pt2

--- client/client_main.c
+++ client/client_main.c
@@ -1,59 +1,26 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
-#include <string.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-
-#define MAX_BUFFER_SIZE 1024
+#include <netdb.h>
+#include <string.h>
 #define AS_ip "tejo.tecnico.ulisboa.pt"
 #define AS_port "58011"
 
-void client_login(const char *userID, const char *password, const char *ASip, const char *ASport) {
-    int sockfd, errcode;
-    ssize_t n;
-    struct addrinfo hints, *res;
+
+void client_login(char *userID,char *password, const char *ASip, const char *ASport) {
 
     // Create message
-    char msg[256];
-    snprintf(msg, sizeof(msg), "LIN %s %s", userID, password);
+    
 
-    // Create socket
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd == -1) {
-        printf("Error creating socket\n");
-        exit(1);
-    }
 
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET; // IPv4
-    hints.ai_socktype = SOCK_DGRAM; // UDP socket
 
-    // Get server
-    errcode = getaddrinfo(AS_ip, AS_port, &hints, &res);
-    if (errcode == -1) {
-        printf("Error getting address info\n");
-        exit(1);
-    }
 
-    n = sendto(sockfd, msg, strlen(msg), 0, res->ai_addr, res->ai_addrlen);
-    if (n == -1) {
-        printf("Error sending message\n");
-        exit(1);
-    }
-
-    char buffer2[128];
-    n = recvfrom(sockfd, buffer2, sizeof(buffer2), 0, NULL, NULL);
-    if (n == -1) {
-        printf("Error receiving message\n");
-        exit(1);
-    }
-
-    printf("%s\n", buffer2);
-    freeaddrinfo(res);
-    close(sockfd);
 }
-
+/* 
 void myauctions(const char *userID, const char *ASip, const char *ASport) {
     int clientSocket;
     struct sockaddr_in serverAddr;
@@ -90,40 +57,66 @@ void myauctions(const char *userID, const char *ASip, const char *ASport) {
 
     close(clientSocket);
 }
-
-int main() {
+ */
+int main(int argc, char *argv[]){
     int check = 1;
-    char command[10], keywords[5][256], userID[256], password[256];
+    char command[128], keywords[5][256],  userID[6], password[256];
+    
+    //define udp or tcp variables
+    int fd, errcode;
+    ssize_t n;
+    socklen_t addrlen;
+    struct addrinfo hints, *res;
+    struct sockaddr_in addr;
+    char buffer[128];
+    
+    
+    if (argc != 1) {
+        const char *as_ip = argv[2];
+        const char *as_port = argv[4];
+    }
+
 
     while (check) {
-        scanf("%s", command);
-        getchar(); // To consume the newline character after reading command
-        fgets(keywords[0], sizeof(keywords[0]), stdin);
-
+        fgets(command, sizeof(command), stdin);        
+        char *token = strtok(command, " ");
         int i = 0;
-        char *token = strtok(keywords[0], " \n");
-        while (token != NULL && i < 5) {
-            strcpy(keywords[i + 1], token);
+        while (token != NULL) {
+            strcpy(keywords[i], token);
+            token = strtok(NULL, " ");
             i++;
-            token = strtok(NULL, " \n");
         }
 
-        if (strcmp(command, "login") == 0) {
+        //create socket        
+        fd = socket(AF_INET, SOCK_DGRAM, 0);
+        if (fd == -1) exit(1);
+        memset(&hints, 0, sizeof hints);
+        hints.ai_family = AF_INET;
+        hints.ai_socktype = SOCK_DGRAM;
+        hints.ai_flags = AI_NUMERICSERV;
+        errcode = getaddrinfo(AS_ip, AS_port, &hints, &res);
+        if (errcode != 0) exit(1);
+
+
+        //check commands
+
+        if (strcmp(keywords[0], "login") == 0) {
             strcpy(userID, keywords[1]);
             strcpy(password, keywords[2]);
+            printf("userID: %s Password: %s\n", userID, password);
             client_login(userID, password, AS_ip, AS_port);
             //TODO: verificar se funciona a 100%;
         } 
         // Other command handling here...
 
-        else if (strcmp(command, "myauctions") == 0 || strcmp(command, "ma") == 0) {
+        else if (strcmp(keywords[0], "myauctions") == 0 || strcmp(keywords[0], "ma") == 0) {
             strcpy(userID, keywords[1]);
             strcpy(password, keywords[2]);
-            myauctions(userID, AS_ip, AS_port);
-            //TODO: ver se está a funcionar bem
+            /* myauctions(userID, AS_ip, AS_port);
+             *///TODO: ver se está a funcionar bem
         }
 
-        else if (strcmp(command, "exit") == 0) {
+        else if (strcmp(keywords[0], "exit") == 0) {
             check = 0;
         }
     }
