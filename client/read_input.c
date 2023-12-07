072fc3a5716e68d69bc78bebfbd07c32646e48b7
Andre <andre.d.oliveira@tecnico.ulisboa.pt>
2023-12-07 13:20:14 +0000

    .

--- client/read_input.c
+++ client/read_input.c
@@ -33,9 +33,32 @@ int read_input(char* buffer, char* input, char* userID, char* password){
         return UDP;
     }
 
+    else if (!strcmp(command, "open")){
+
+
+    }
+    else if(!strcmp(command, "close")){
+
+    }
     else if (!strcmp(command, "myauctions") || !strcmp(command, "ma")) {
         /* myauctions(userID, AS_ip, AS_port);
             *///TODO ver se está a funcionar bem
+    }
+
+    else if(!strcmp(command, "mybids") || !strcmp(command, "mb")){
+
+    }
+
+    else if(!strcmp(command, "list") || !strcmp(command, "l")){
+
+    }
+
+    else if(!strcmp(command, "show_asset") || !strcmp(command, "sa")){
+
+    }
+
+    else if(!strcmp(command, "show_record") || !strcmp(command, "sr")){
+        
     }
 
     else if (!strcmp(command, "exit\n")) {
